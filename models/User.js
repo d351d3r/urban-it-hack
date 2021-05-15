296f9ddd51026b9ff5700227898ba70bcaafa48c
Fedor Ishchenko <aleks180700@gmail.com>
2021-05-15 19:20:27 +1000

    user + auth

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const {Schema, model, ObjectId} = require("mysql2")
+
+
+const User = new Schema({
+    id:{type:Number, require: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    verificated: {type: Boolean, required: true},
+    created_date: {type: String, required: true},
+    name: {type: String, required: true},
+    XP: {type: Number, required: true},
+    currentBalance: {type: Number, required: true},
+    level: {type: Number, required: true}
+})
+
+module.exports = model('User', User)
