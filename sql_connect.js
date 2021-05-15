3bdc34afe42d750aa382cfe659faf8e236832a40
Anatolii Cherednichenko <greatestcombinator@gmail.com>
2021-05-15 19:03:12 +1000

    Add SQL connection

--- sql_connect.js
+++ sql_connect.js
@@ -1,22 +1,39 @@
-function init(){
-    const mysql      = require('mysql');
-    const connection = mysql.createConnection({
-        host     : '192.168.8.190',
-        user     : 'urban1',
-        database: "urban1",
-        password : 'O9jGF8'
+function init() {
+    const mysql = require('mysql');
+
+// Get the Host from Environment or use default
+    const host = process.env.DB_HOST || '192.168.8.190';
+
+// Get the User for DB from Environment or use default
+    const user = process.env.DB_USER || 'urban1';
+
+// Get the Password for DB from Environment or use default
+    const password = process.env.DB_PASS || 'O9jGF8';
+
+// Get the Database from Environment or use default
+    const database = process.env.DB_DATABASE || 'urban1';
+
+// Create the connection with required details
+    const con = mysql.createConnection({
+        host, user, password, database,
     });
 
-    connection.connect();
+    const query = "SELECT * FROM users";
 
-    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+// make to connection to the database.
+    con.connect(function (err) {
         if (err) throw err;
-        console.log('The solution is: ', rows[0].solution);
+
+        // if connection is successful
+        con.query(query, (err, result, fields) => {
+            // if any error while executing above query, throw error
+            if (err) throw err;
+
+            // if there is no error, you have the result
+            console.log(result);
+        });
     });
-
-    connection.end();
 }
-
 module.exports = {
     init
 }
